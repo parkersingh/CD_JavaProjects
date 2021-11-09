fc11860705ec7d03b5b9cac99a7c51e92db2b3a2
parkersingh <rekrapsingh@gmail.com>
2021-11-08 22:14:00 -0800

    Started HashMapJava

---
+++ HashMap_Exercise/HashMapJava.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Set;
+
+public class HashMapJava {
+    public static void main(String[] args) {
+        HashMap<String, String> trackList = new HashMap<String, String>();
+        trackList.put("Let it Go", "The snow glows white on the mountain tonight");
+        trackList.put("I'll Make a Man Out of You", "Tranquil as a forest, but on fire within");
+    }
+}
